31758e6a31a53833fdf73a70acf7b4888fd37e3d
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-06-17 22:36:48 +0630

    17 june

---
+++ .history/lesson/exerciseone/resources/views/employees/index.blade_20230617204655.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>Employee Page </title>
+      <p>This is Employee Home</p>
+</head>   
+<body>
+    <h1>Welcome to Our blade site </h1>
+
+    <?php
+    //    foreach($employeesdata as $value){
+    //     echo $value;
+    //    }
+    ?>
+
+    <!-- @php
+       foreach($employeesdata as $value){
+        echo $value;
+       }
+    @endphp -->
+
+    <ul>
+        @foreach($employeesdata as $value)
+           <!-- <li>{!!$value!!} </li> -->
+           <li>{!!$value!!}</li>
+        @endforeach
+
+    </ul>
+
+    @php 
+
+    $city = "Mandalay"
+
+    @endphp
+
+    @if($city === "yangon")
+            <h3>Correct</h3>
+        @else 
+        <h3> You were wrong <h3>
+
+    @endif
+
+    {{$thanks}}
+
+
+    {{$services}}
+
+</body> 
+</html>
+
+<!-- 28EN  -->
