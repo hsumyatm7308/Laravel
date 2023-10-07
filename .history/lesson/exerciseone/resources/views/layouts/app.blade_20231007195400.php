3b272713d81944fdae91abb34508b0aa16945003
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-10-07 21:22:51 +0630

    7 10 2023

---
+++ .history/lesson/exerciseone/resources/views/layouts/app.blade_20231007195400.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <!-- boostrap css1 js1  -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+         <!-- fontawesome  -->
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    </head>
+    <body>
+
+    <div class="container">
+
+       @yield('content')
+
+    </div>
+
+
+    <div class="container">
+        Copyright 2023. Design by ABC Co.,Ltd 
+    </div>
+        
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+      <script src="./layouts/jq.js"></script>
+
+      @yield('script')
+    </body>
+</html>
---
+++ .history/lesson/exerciseone/resources/views/products/index.blade_20231007204518.php
@@ -0,0 +1,86 @@
+@extends('layouts.app')
+
+@section('title','Index Page')
+@section('content')
+
+<h1>Index Page</h1>
+
+<div class="col-md-12 container mb-3"> <a href="{{route('countries.create')}}" class="btn btn-primary btn-sm rounded-0">
+    Create New Country</a>
+    </div> <div class="col-md-12">
+
+    <table class="table table-sm table-hover border container"> <thead>
+        <tr>
+        <th>No</th>
+        <th>Name</th>
+        <th>Image</th>
+        <th>Price</th>
+        <th>User ID</th>
+        <th>Created At</th>
+        <th>Updated At</th>
+        <th>Action </th>
+        </tr>
+
+        </thead>
+        <tbody> @foreach($products as $idx=>$product) <tr>
+            <td>{{++$idx}}</td>
+            <td><a href="{{route('countries.show',$country->id)}}">{{$country->name}}</a></td>
+            <td>{{$product->name}}</td>
+            <td>{{$product->price}}</td>
+            <td>{{$product->image}}</td>
+            <td>{{$product->created_at}}</td>
+            <td>{{$product->updated_at}}</td>
+            <td class="d-flex justify-content-center align-items-center">
+
+
+
+
+            {{-- <a href="{{url('countries',$product['id'])}}" class="text-danger"><i class="fas fa-trash-alt"></i></a>
+            --}}
+
+            <a href="{{route('products.edit',$product->id)}}" class="text-info m-3"><i class="fas fa-pen"></i></a>
+
+            <form class="formdelete" action="{{route('products.destroy',$country->id)}}" method="POST" class="d-flex
+            justify-content-center align-items-center">
+            @csrf
+            @method("DELETE")
+            <button type="submit" class="text-danger"><i class="fas fa-trash-alt"></i></button>
+
+            </form>
+            </td>
+
+            <td>
+                <a href="#" class="btn btn-danger"
+                onclick="event.preventDefault();document.getElementById('formdelete{{$product->id}}').submit();">Delete</a>
+                </td>
+                <form id="formdelete{{$product->id}}" action=" {{url('/products',$product['id'])}}" method="POST">
+                    {{ csrf_field()}}
+                    {{method_field('DELETE')}}
+                </form>
+                </tr>
+                @endforeach
+                </tbody>
+
+    </table>
+
+</div>
+
+@section('footer')
+
+
+@section('script')
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.formdelete').on('submit', function () {
+            if (confirm('Are you sure want to delete it?')) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+    })
+
+</script>
+
+
+@endsection
